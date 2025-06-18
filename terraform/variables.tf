7726d54cec389d8b888f298cade56aa440b4cd74
nishantnesargi99 <nishant.nesargi@datacultr.com>
2025-06-18 17:09:12 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,8 +2,3 @@ variable "existing_key_name" {
   description = "Name of existing EC2 Key Pair"
   type        = string
 }
-variable "security_group_id" {
-  description = "ID of existing security group"
-  type        = string
-}
-
