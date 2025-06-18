e2b9846a94a77d8cf6e0c07d5157245ec8d88055
nishantnesargi99 <nishant.nesargi@datacultr.com>
2025-06-18 14:47:32 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,10 +2,6 @@ variable "existing_key_name" {
   description = "Name of existing EC2 Key Pair"
   type        = string
 }
-variable "private_key_path" {
-  description = "Path to SSH private key (PEM file)"
-  type        = string
-}
 variable "security_group_id" {
   description = "ID of existing security group"
   type        = string
