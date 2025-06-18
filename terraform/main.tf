786f8152c4a69d96a6cdef2829102b25dc4beb56
nishantnesargi99 <nishant.nesargi@datacultr.com>
2025-06-18 17:16:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,27 @@
 provider "aws" {
-  region = "us-east-2"  # Change if using a different region
+  region = "us-east-2"
 }
 # Get the default VPC
 data "aws_vpc" "default" {
   default = true
 }
-# Get a subnet from the default VPC 
-data "aws_subnet" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-  # Optional: Limit to public subnets
-  filter {
-    name   = "default-for-az"
-    values = ["true"]
-  }
+# Get all subnet IDs from the default VPC
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
 }
-# Get the default security group of the default VPC
+# Get all subnet details (one for each ID)
+data "aws_subnet" "selected" {
+  for_each = toset(data.aws_subnet_ids.default.ids)
+  id       = each.value
+}
+# Filter subnet IDs by specific AZ: us-east-2a
+locals {
+  selected_subnet_id = [
+    for s in data.aws_subnet.selected :
+    s.id if s.availability_zone == "us-east-2a"
+  ][0]
+}
+# Use the default security group of the VPC
 data "aws_security_group" "default" {
   filter {
     name   = "vpc-id"
@@ -27,20 +31,19 @@ data "aws_security_group" "default" {
     name   = "group-name"
     values = ["default"]
   }
-availability_zone = "us-east-2a"
 }
-# EC2 instance resource
+# Create the EC2 instance
 resource "aws_instance" "app_server" {
-  ami                    = "ami-019eeff96c2865995"  # Replace with valid AMI ID for your region
+  ami                    = "ami-019eeff96c2865995"
   instance_type          = "t3.micro"
   key_name               = var.existing_key_name
-  subnet_id              = data.aws_subnet.default.id
+  subnet_id              = local.selected_subnet_id
   vpc_security_group_ids = [data.aws_security_group.default.id]
   tags = {
     Name = "AppServerFromJenkins"
   }
 }
-# Output public IP
+# Output public IP of the EC2 instance
 output "instance_ip" {
   value = aws_instance.app_server.public_ip
 }
