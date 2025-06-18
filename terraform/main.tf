1c4cfddb06aade19d4df313777719ab8909b4acf
nishantnesargi99 <nishant.nesargi@datacultr.com>
2025-06-18 14:50:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "app_server" {
   ami                    = "ami-019eeff96c2865995"  
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   key_name               = var.existing_key_name     # Use existing EC2 key pair
   subnet_id              = var.subnet_id             # Optional if you use default VPC
   vpc_security_group_ids = [var.security_group_id]   # Attach existing SG
