0d6919cf531b738baeb5131f2f85ccd472ad8d44
nishantnesargi99 <nishant.nesargi@datacultr.com>
2025-06-18 17:18:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,23 +5,25 @@ provider "aws" {
 data "aws_vpc" "default" {
   default = true
 }
-# Get all subnet IDs from the default VPC
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+# Filter a single subnet in that VPC and in desired AZ
+data "aws_subnet" "default_az2a" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-2a"]
+  }
+  # optional: ensure only subnets that are available
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+  # This ensures you get just one match
+  most_recent = true
 }
-# Get all subnet details (one for each ID)
-data "aws_subnet" "selected" {
-  for_each = toset(data.aws_subnet_ids.default.ids)
-  id       = each.value
-}
-# Filter subnet IDs by specific AZ: us-east-2a
-locals {
-  selected_subnet_id = [
-    for s in data.aws_subnet.selected :
-    s.id if s.availability_zone == "us-east-2a"
-  ][0]
-}
-# Use the default security group of the VPC
+# Use the default security group for that VPC
 data "aws_security_group" "default" {
   filter {
     name   = "vpc-id"
@@ -32,18 +34,17 @@ data "aws_security_group" "default" {
     values = ["default"]
   }
 }
-# Create the EC2 instance
+# Create EC2 instance
 resource "aws_instance" "app_server" {
   ami                    = "ami-019eeff96c2865995"
   instance_type          = "t3.micro"
   key_name               = var.existing_key_name
-  subnet_id              = local.selected_subnet_id
+  subnet_id              = data.aws_subnet.default_az2a.id
   vpc_security_group_ids = [data.aws_security_group.default.id]
   tags = {
     Name = "AppServerFromJenkins"
   }
 }
-# Output public IP of the EC2 instance
 output "instance_ip" {
   value = aws_instance.app_server.public_ip
 }
