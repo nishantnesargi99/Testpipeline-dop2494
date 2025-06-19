f283673c36af9212c14bcda66648746f51e316bd
nishantnesargi99 <nishant.nesargi@datacultr.com>
2025-06-19 15:58:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,52 @@
 provider "aws" {
   region = "us-east-2"
 }
+# :mag: Get VPC ID from the provided subnet_id
+data "aws_subnet" "selected" {
+  id = var.subnet_id
+}
+# :white_check_mark: Create a new SG using that VPC ID
+resource "aws_security_group" "jenkins_app_sg" {
+  name        = "jenkins-app-sg"
+  description = "Security group for Jenkins EC2 with SSH on 9211"
+  vpc_id      = data.aws_subnet.selected.vpc_id
+  ingress {
+    description = "Allow SSH on port 9211"
+    from_port   = 9211
+    to_port     = 9211
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "jenkins-app-sg"
+  }
+}
+# :white_check_mark: EC2 Instance
 resource "aws_instance" "app_server" {
   ami                         = "ami-019eeff96c2865995"
   instance_type               = "t4g.micro"
   subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = [var.security_group_id]  # Use existing SG
+  vpc_security_group_ids      = [aws_security_group.jenkins_app_sg.id]
   key_name                    = var.existing_key_name
   user_data = <<-EOF
               #!/bin/bash
@@ -17,26 +58,7 @@ resource "aws_instance" "app_server" {
     Name = "AppServerFromJenkins"
   }
 }
-# :white_check_mark: Add SSH port 9211 to existing security group
-resource "aws_security_group_rule" "allow_ssh_9211" {
-  type              = "ingress"
-  from_port         = 9211
-  to_port           = 9211
-  protocol          = "tcp"
-  security_group_id = var.security_group_id
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "Allow SSH on custom port 9211"
-}
-# (Optional) Add HTTP rule if needed
-resource "aws_security_group_rule" "allow_http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = var.security_group_id
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "Allow HTTP"
-}
+# :white_check_mark: Output for Jenkins
 output "instance_ip" {
   value = aws_instance.app_server.public_ip
 }
