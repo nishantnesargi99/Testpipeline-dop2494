1b47153a08ab16e983aa3c2e67dc57d32e7e5ac6
nishantnesargi99 <nishant.nesargi@datacultr.com>
2025-06-19 10:14:51 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,12 @@
+variable "subnet_id" {
+  description = "ID of the existing Subnet where the EC2 instance will be launched"
+  type        = string
+}
+variable "security_group_id" {
+  description = "ID of the existing Security Group to associate with the EC2 instance"
+  type        = string
+}
 variable "existing_key_name" {
-  description = "Name of existing EC2 Key Pair"
+  description = "Name of the existing EC2 Key Pair to use for SSH access"
   type        = string
 }
